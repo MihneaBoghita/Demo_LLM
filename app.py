5282ae59abfe47244f41bdcb4eb2c1369607825e
Iovu Rares <raresciovu@gmail.com>
2026-03-04 23:09:25 +0200

    Added delete method

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask, jsonify, request, Response
 
 app = Flask(__name__, template_folder="templates")
 
+
 produse = [
     {"id": 1, "nume": "Laptop", "pret": 3500},
     {"id": 2, "nume": "Mouse", "pret": 150}
 ]
+next_id = 3
 
 @app.route('/produse', methods=['GET']) 
 def get_produse():
@@ -18,14 +20,25 @@ def get_produs(produs_id):
         return jsonify({"eroare": "Produsul nu a fost găsit"}), 404
     return jsonify(rezultat[0])
 
+@app.route('/delete_produs/<int:produs_id>', methods=['DELETE'])
+def delete_produs(produs_id):
+    rezultat = next((p for p in produse if p['id'] == produs_id), None)
+    if not rezultat:
+        return jsonify({"eroare": "Produsul nu a fost găsit"}), 404
+    produse.remove(rezultat)
+    return jsonify({"message": "Produsul a fost sters"}), 200
+
 @app.route('/add_produs/', methods=['POST'])
 def add_produs():
+    global next_id
+
     nou_produs = {
-        "id": len(produse) + 1,
+        "id": next_id,
         "nume": request.json['nume'],
         "pret": request.json['pret']
     }
     produse.append(nou_produs)
+    next_id += 1
     return jsonify(nou_produs), 201
 
 
