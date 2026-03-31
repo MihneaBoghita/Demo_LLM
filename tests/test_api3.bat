74d7364843a941259c2423c52a1ed0595914c432
MihneaBoghita <Mihnea.Boghita@gmail.com>
2026-03-31 23:02:22 +0300

    Adaug test_api3.bat

---
+++ tests/test_api3.bat
@@ -0,0 +1,13 @@
+
+
+@echo off
+
+@echo Send to the Server...
+
+curl -X POST http://127.0.0.1:5000/add_produs/ \
+     -H "Content-Type: application/json" \
+     -d '{"name":"printer","price":200}'
+
+echo.
+echo Operatiune finalizata.
+pause
